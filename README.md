d1314d7fffdd5ada99a118368f64424ef775942e
TheSandDoctor <majorjohn1@mail.com>
2019-12-12 19:43:19 -0800

    Final update to formatting, update readme
    Some formatting cleanup, updated readme to reflect that this repo is now abandoned and only intended as a
    partial example.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # redirect_maker
 This was an example project as requested by [Wikipedia user RhinosF1](https://en.wikipedia.org/wiki/Special:Contributions/RhinosF1), prior to their indefinite blocking. This project was to demonstrate how to create [user space](https://en.wikipedia.org/wiki/Wikipedia:User_pages#Terminology_and_page_locations) [redirects](https://en.wikipedia.org/wiki/Wikipedia:Redirect) for each month of the year to their [user page](https://en.wikipedia.org/wiki/User:RhinosF1).
+
+This specific repository is ***largely unmaintained/abandoned***. No further work is planned.
--- redirect_maker.py
+++ redirect_maker.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3.6
-import configparser, json, mwclient
+import configparser
+import json
+import mwclient
 from time import sleep
 from mwclient import errors
 
 
 def save_wrap(res):
-    site,title = res
+    site, title = res
     page = site.Pages[title]
     print(str(page.page_title))
-    #if not page.exists: ##NOT IN USE##
-         #save_edit(page, site, "#REDIRECT [[User:RhinosF1]]")
+    if not page.exists:
+        save_edit(page, site, "#REDIRECT [[User:RhinosF1]]")
     else:
         input("EXISTS " + str(page.page_title))
-        if 'Y':
-           save_edit(page, site, "#REDIRECT [[
-        else:
-           print('Change Refused')
 
 
 def save_edit(page, site, text)
@@ -38,7 +36,7 @@ def save_edit(page, site, text)
 
 
 def gen_page_titles(month, month_dict, year):
-    return "User:RhinosF1/Archives_" + str(year) + "/" + str(month) + "_" + "(" + month_dict.get(month) + ")"
+    return 'User:RhinosF1/Archives_' + str(year) + '/' + str(month) + '_' + '(' + month_dict.get(month) + ')'
 
 
 def main():
@@ -46,29 +44,30 @@ def main():
     config = configparser.RawConfigParser()
     config.read('credentials.txt')
     try:
-        site.login(config.get('enwiki_sandbot', 'username'), config.get('enwiki_sandbot', 'password'))
+        site.login(config.get('section_header', 'username'), config.get('section_header', 'password'))
     except errors.LoginError as e:
         print(e)
-        raise ValueError("Login failed.")
+        raise ValueError('Login failed.')
 
     month_dict = {
-        1: "January",
-        2: "February",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December'
     }
-    # since ranges are not inclusive, end must always be +1 above
+    # Since ranges are not inclusive, end must always be +1 above
     lst = [gen_page_titles(month, month_dict, year) for year in range(2018, 2019) for month in range(1, 13)]
     for i in map(save_wrap, [(site, x) for x in lst]):
         pass
+
 
 if __name__ == "__main__":
     main()
